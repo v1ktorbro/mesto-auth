554f131ffe76ece995038e669e415cbfed3b614e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-09-16 09:55:44 +0500

    refactor: auth.js from component to src

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
-import * as auth from './auth';
+import * as auth from '../auth';
 import InfoTooltip from './InfoTooltip';
 
 function Login ({ handleLogin }) {
