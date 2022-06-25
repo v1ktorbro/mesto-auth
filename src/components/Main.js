5b7ba367fd0a50a88465878cf8bb462617ec7a46
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-25 15:07:58 +0500

    refactor(Main): remove eslint rules

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import Card from './Card';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
