2b79d48f208c9562ead70809a0512f54e4d8bafb
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-25 14:43:37 +0500

    refactor(auth): update url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5001';
+export const BASE_URL = 'https://mesto-auth.abrosimov.site/api';
 
 export const register = ({ email, password }) => {
   return fetch(`${BASE_URL}/signup`, {
